bd6cd09b168f7c3e3e88b2468a57f09cef2caf51
Anne van Rossum <anne@almende.org>
2012-09-25 10:06:53 +0200

    Update script with old libs that are forgotten to be installed

--- RoombaModule/scripts/manual_install.sh
+++ RoombaModule/scripts/manual_install.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+# Navigate to the parent directory of MADP, and execute "make" as well as "make install"
+# now there are some files that have been forgotten to be installed.
+# Make sure there is a "src" directory where you are, on my system the full path is:
+#   SOURCE=/home/anne/myworkspace/roomba/MADPtrunk20120918/src
+# and now execute ./manual_install.sh
+#
+# Copyright (c) Anne C. van Rossum
+# Almende B.V. / DO bots
+# License: LGPLv3
+#
+
+SOURCE=src
+
+HEADER_PATH=/usr/local/include/madp
+LIB_PATH=/usr/local/lib
+
 sudo mkdir -p /usr/local/include/madp
 
-sudo rsync -avzul src/support/*.h /usr/local/include/madp/support/
-sudo rsync -avzul src/planning/*.h /usr/local/include/madp/planning/
-sudo rsync -avzul src/base/*.h /usr/local/include/madp/base/
-sudo rsync -avzul src/include/*.h /usr/local/include/madp/
+sudo rsync -avzul ${SOURCE}/support/*.h ${HEADER_PATH}/support/
+sudo rsync -avzul ${SOURCE}/planning/*.h ${HEADER_PATH}/planning/
+sudo rsync -avzul ${SOURCE}/base/*.h ${HEADER_PATH}/base/
+sudo rsync -avzul ${SOURCE}/include/*.h ${HEADER_PATH}/
+
+sudo cp ${SOURCE}/libpomdp-solve/src/mdp/libmdp.a ${LIB_PATH}
+sudo cp ${SOURCE}/libpomdp-solve/src/mdp/libmdp.a ${LIB_PATH}
