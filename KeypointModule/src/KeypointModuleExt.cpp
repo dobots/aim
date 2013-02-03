cd41aaa02a12d66b2e143d9716f6721d592a1a28
Anne van Rossum <anne@almende.org>
2013-02-03 12:48:24 +0100

    Include calib3d for calculation of fundamental matrix

--- KeypointModule/src/KeypointModuleExt.cpp
+++ KeypointModule/src/KeypointModuleExt.cpp
@@ -1,6 +1,7 @@
 #include <KeypointModuleExt.h>
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 
 #include <sstream>
 #include <assert.h>
@@ -20,6 +21,10 @@ void KeypointModuleExt::Prepare() {
 	dm = DescriptorMatcher::create("BruteForce-Hamming");
 }
 
+/**
+ * Get keypoints and descriptors from one image. If the image is larger than
+ * 640x480 we will only take a region of interest that size.
+ */
 void KeypointModuleExt::GetKeypoints(Mat & image, std::vector<KeyPoint> & keypoints, Mat & descriptors) {
 	Mat roi(image.size(), CV_8UC1, Scalar(0));
 	Point poly[] = {Point(0, 0), Point(0, 640), Point(480, 640), Point(480, 0)};
@@ -29,7 +34,6 @@ void KeypointModuleExt::GetKeypoints(Mat & image, std::vector<KeyPoint> & keypoi
 	fd->detect(image, keypoints, roi);
 	de->compute(image, keypoints, descriptors);
 
-	int roiViolations = 0;
 	for(std::vector<KeyPoint>::const_iterator kp = keypoints.begin(); kp != keypoints.end(); ++kp)
 	{
 		int x = cvRound(kp->pt.x);
@@ -41,6 +45,11 @@ void KeypointModuleExt::GetKeypoints(Mat & image, std::vector<KeyPoint> & keypoi
 	printf("Found %i keypoints\n", (int)keypoints.size());
 }
 
+/**
+ * Helper struct to store things that have to do with features and descriptors.
+ * It uses a RawImage class to load data into an OpenCV image, so we do not need
+ * the highgui library of OpenCV.
+ */
 struct Img {
 	Img(int width, int height) {
 		image = new Mat(height,width,CV_8UC1);
@@ -64,6 +73,11 @@ struct Img {
 	CRawImage *rawimg;
 };
 
+/**
+ * Takes two subsequent images, finds features, and calculates descriptors at these places.
+ * Then an exhaustive search across the descriptors by k-nearest neighbour, finds in the
+ * end descriptors that match and are not too far apart.
+ */
 void KeypointModuleExt::Tick() {
 	std::vector<Img*> images;
 	int width = 640, height = 480;
@@ -95,26 +109,44 @@ void KeypointModuleExt::Tick() {
 	}
 	printf("Found %i matches\n", matchCount);
 
+	// Convert keypoints into Point2f
+	std::vector<cv::Point2f> selPoints1, selPoints2;
+	std::vector<int> pointIndexes1, pointIndexes2;
+	cv::KeyPoint::convert(images[0]->keypoints,selPoints1,pointIndexes1);
+	cv::KeyPoint::convert(images[1]->keypoints,selPoints2,pointIndexes2);
+	// Compute F matrix from 7 matches
+	cv::Mat fundamental= cv::findFundamentalMat(
+	   cv::Mat(selPoints1), // points in first image
+	   cv::Mat(selPoints2), // points in second image
+	   CV_FM_7POINT);       // 7-point method
+//	  cv::Mat fundamental= ransacTest(symMatches,
+//			  images[0]->keypoints, images[1]->keypoints, matches);
+
+#ifdef DRAW_RESULTS
 	Mat *result = new Mat(height,width*2,CV_8UC1);
 	const vector<vector<char> > mask;
 	drawMatches(*images[0]->image, images[0]->keypoints, *images[1]->image, images[1]->keypoints, matches, *result,
 			 Scalar::all(-1), Scalar::all(-1), mask, DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
 	CRawImage *rawimg = new CRawImage(width*2,height,3);
 	rawimg->data = result->data;
 	rawimg->saveBmp("result.bmp");
+	delete rawimg;
+	delete result;
+#endif
 
 	for (int i=0; i < 2; ++i) {
 		delete images[i];
 	}
 }
 
+/**
+ * The OpenCV classes are referenced by smart pointers, so do not need to be deallocated
+ * manually.
+ */
 bool KeypointModuleExt::Stop() {
 	if (--lifetime) {
 		return false;
 	}
-//	delete fd;
-//	delete de;
 	return true;
 }
 
