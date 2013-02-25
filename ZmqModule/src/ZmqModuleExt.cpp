db0738c1f820e746536f51310dce03639e8db832
Anne van Rossum <anne@almende.org>
2013-02-25 18:24:06 +0100

    Ports received properly, json decoded

--- ZmqModule/src/ZmqModuleExt.cpp
+++ ZmqModule/src/ZmqModuleExt.cpp
@@ -6,10 +6,15 @@
 #include <iostream>
 #include <unistd.h>
 
+#include <json_spirit_reader.h>
+
 using namespace rur;
 using namespace std;
 
 static int lifetime = 10;
+
+
+static const string Value_type_str[] = { "obj_type", "array_type", "str_type", "bool_type", "int_type", "real_type", "null_type" };
 
 ZmqModuleExt::ZmqModuleExt(): context(1), socket(NULL), server(false) {
 }
@@ -44,15 +49,55 @@ void ZmqModuleExt::Init(std::string & name) {
 			socket->send(request);
 
 			zmq::message_t reply;
-			//reply.rebuild();
 			if (!socket->recv (&reply)) continue;
 			size_t msg_size = reply.size();
 			char* address = new char[msg_size+1];
 			memcpy (address, (void *) reply.data(), msg_size);
 			address[msg_size] = '\0';
-			std::cout << "Received " << string(address) << std::endl;
+			std::string json = string(address);
+			std::cout << "Received " << json << std::endl;
 			delete [] address;
 
+			zmq::socket_t(context, ZMQ_REP);
+			bool valid;
+			json_spirit::Value value;
+			if (!json_spirit::read(json, value)) {
+				valid = false;
+				std::cerr << "Not a json value" << std::endl;
+				return;
+			}
+			if (value.type() != json_spirit::obj_type) {
+				cerr << "[1] Unexpected object type \"" << Value_type_str[value.type()] << "\"";
+				cerr << " instead of \"" << Value_type_str[json_spirit::obj_type] << "\"" << endl;
+				return;
+			}
+
+			// a "json_spirit object" is - by default - a vector of json pairs
+			json_spirit::Object obj = value.get_obj();
+
+			std::string json_server;
+			std::string json_port;
+
+			for( json_spirit::Object::size_type i = 0; i != obj.size(); ++i ) {
+				const json_spirit::Pair& pair = obj[i];
+				const string& key = pair.name_;
+				const json_spirit::Value& value = pair.value_;
+//				cout << "Retrieve json object with name \"" << key << "\"" << " and type " << Value_type_str[value.type()] << endl;
+
+				if (key == "identifier") {
+					// same thing
+				} else if (key == "server") {
+					json_server = value.get_str();
+				} else if (key == "port") {
+					json_port = value.get_str();
+				}
+			}
+
+			std::stringstream ss;
+			ss << "tcp://" << json_server << ":" << json_port;
+			std::string sock = ss.str();
+			std::cout << "Bind to socket " << sock << std::endl;
+			socket->bind(sock.c_str());
 		}
 	}
 }
