32f3bd38d680856ed2a5074a421c1e05a4dfabe2
Anne van Rossum <anne@almende.org>
2013-03-11 17:00:04 +0100

    Set default generated file to STANDARD

--- example/sum/SumModule.h
+++ example/sum/SumModule.h
@@ -9,27 +9,15 @@
  * bio-industry, for animal experimentation, or anything that violates the Universal
  * Declaration of Human Rights.
  *
- * Copyright (c) 2010 Anne van Rossum <anne@almende.com>
+ * Copyright (c) 2012 Anne van Rossum <anne@almende.com>
  *
  * @author  Anne C. van Rossum
  * @company Almende B.V.
  */
 
-// general C/C++ headers
-#include <vector>
 #include <string>
-#include <sstream>
+#include <vector>
 
-// middleware specific headers
-#include <yarp/os/BufferedPort.h>
-#include <yarp/os/Network.h>
-#include <yarp/os/Bottle.h>
-
-// namespaces and typedefs
-using namespace yarp::os;
-
-// recommended namespace: "rur"
-// do not forget to add "using namespace rur;" to your .cpp file
 namespace Rur {
 
 struct Param {
@@ -38,138 +26,50 @@ struct Param {
 
 typedef std::vector<double> long_seq;
 
-// The generated class. Do not modify or add class members
-// Either derive from this class and implement Tick() or
-// use a separate helper class to store state information.
-// All information for the operation of the module should 
-// be obtained over the defined ports
 class SumModule {
 private:
-  Network yarp;
-  std::string module_id;
   
-  // private storage for portInputValues;
-  std::vector<double> *portInputValues;
-  // the port portInput itself
-  BufferedPort<Bottle> *portInput;
+  long_seq dummyInput;
   
-  // the port portOutput itself
-  BufferedPort<Bottle> *portOutput;
   
-  // the port portTest1 itself
-  BufferedPort<Bottle> *portTest1;
   
-  // private storage for portTest2Value
-  int portTest2Value;
-  // the port portTest2 itself
-  BufferedPort<Bottle> *portTest2;
-  
-  // User-defined structs (automatically allocated later)
+  int dummyTest2;
   Param *cliParam;
 
+protected:
+  static const int channel_count = 4;
+  const char* const channel[4] = {"readInput", "writeOutput", "writeTest1", "readTest2"};
+
 public:
-  // The constructor needs to be called, also when you derive from this class
   SumModule() {
+    dummyInput = long_seq(0);
+    dummyTest2 = int(0);
     cliParam = new Param();
-    portInputValues = new std::vector<double>();
-    portInput = new BufferedPort<Bottle>();
-    portOutput = new BufferedPort<Bottle>();
-    portTest1 = new BufferedPort<Bottle>();
-    portTest2 = new BufferedPort<Bottle>();
   }
   
-  ~SumModule() {
-    delete portInputValues;
-    delete portInput;
-    delete portOutput;
-    delete portTest1;
-    delete portTest2;
-    delete cliParam;
-  }
+  ~SumModule() { }
   
-  // This is the function you will need to implement.
-  void Tick(); 
+  void Tick();
   
-  bool Stop(); 
+  bool Stop();
   
-  
-  // After construction you will need to call this function first
-  // it opens the YARP ports
-  void Init(std::string module_id) {
-    this->module_id = module_id;
-    
-    {
-      std::stringstream portName; portName.str(); portName.clear();
-      portName << "/summodule" << module_id << "/input";
-      portInput->open(portName.str().c_str());
-    }
-    {
-      std::stringstream portName; portName.str(); portName.clear();
-      portName << "/summodule" << module_id << "/output";
-      portOutput->open(portName.str().c_str());
-    }
-    {
-      std::stringstream portName; portName.str(); portName.clear();
-      portName << "/summodule" << module_id << "/test1";
-      portTest1->open(portName.str().c_str());
-    }
-    {
-      std::stringstream portName; portName.str(); portName.clear();
-      portName << "/summodule" << module_id << "/test2";
-      portTest2->open(portName.str().c_str());
-    }
-  }
-  
-  // Before destruction you will need to call this function first
-  // it closes the YARP ports
-  void Close() {
-    portInput->close();
-    portOutput->close();
-    portTest1->close();
-    portTest2->close();
-  }
+  void Init(std::string & name) { }
   
   // Function to get Param struct (to subsequently set CLI parameters)
   inline Param *GetParam() { return cliParam; };
   
-protected:
-  // All subsequent functions should be called from "within" this module
-  // From either the Tick() routine itself, or Tick() in a derived class
-  
-  // Remark: caller is responsible for evoking vector.clear()
-  inline std::vector<double> *readInput(bool blocking=true) {
-    Bottle *b = portInput->read(blocking);
-    if (b != NULL) { 
-      for (int i = 0; i < b->size(); ++i) {
-        portInputValues->push_back(b->get(i).asDouble());
-      }
-    }
-    return portInputValues;
+  inline long_seq *readInput() {
+    return &dummyInput;
   }
   
   inline void writeOutput(const int data_out) {
-    Bottle &data_outPrepare = portOutput->prepare();
-    data_outPrepare.clear();
-    data_outPrepare.addInt(data_out);
-    portOutput->write(true);
   }
   
-  inline void writeTest1(const long_seq &test1) {
-    Bottle &test1Prepare = portTest1->prepare();
-    test1Prepare.clear();
-    for (int i = 0; i < test1.size(); ++i) {
-      test1Prepare.addDouble(test1[i]);
-    }
-    portTest1->write(true);
+  inline void writeTest1(const long_seq test1) {
   }
   
-  inline int *readTest2(bool blocking=true) {
-    Bottle *b = portTest2->read(blocking);
-    if (b != NULL) { 
-      portTest2Value = b->get(0).asInt();
-      return &portTest2Value;
-    }
-    return NULL;
+  inline int *readTest2() {
+    return &dummyTest2;
   }
   
 };
