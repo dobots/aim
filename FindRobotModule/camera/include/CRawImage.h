28aa654c1619f27facbffe4c8f61c157eba04a09
Anne van Rossum <anne@almende.org>
2013-05-14 14:53:15 +0200

    Add text to CRawImage

--- FindRobotModule/camera/include/CRawImage.h
+++ FindRobotModule/camera/include/CRawImage.h
@@ -1,17 +1,40 @@
+/**
+ * @brief Basic image storage and a few functions for manipulation
+ * @file CRawImage.h
+ *
+ * Copyright © 2012 Tom Krajnik
+ *
+ * @author		Tom Krajnik, Anne C. van Rossum
+ * @date		May 14th, 2013
+ * @institute	Czech Technical University in Prague
+ * @company		Almende B.V.
+ * @project		Replicator
+ */
+
 #ifndef CIMAGE_H
 #define CIMAGE_H
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-//#define THICK_CROSS
 
+/**
+ * This is a very basic implementation of an image. It just uses three char's, one for each of the color channels.
+ * Nothing fancy, no padding. Moreover, this struct is only used for communication with the user. The internal structure
+ * is a (linear) array with RGB values or monochrome values.
+ */
 struct Pixel {
 	char r;
 	char g;
 	char b;
 };
 
+/**
+ * The patch is another convenient struct for the user. It allows you to get a patch with data from the image. It is
+ * not meant to be super fast, it is just simple. It requires you to set a width and height beforehand, and then it
+ * copies the pixels to the data field. This data field does not need to be allocated by you, this will be done in this
+ * compilation unit.
+ */
 struct Patch {
 	unsigned char *data;
 	int width;
@@ -19,84 +42,109 @@ struct Patch {
 };
 
 /**
-@author Tom Krajnik
-*/
-//template <typename T>
+ * @author Tom Krajnik
+ * @author Anne C. van Rossum
+ */
 class CRawImage {
 public:
 
-  CRawImage(int wi, int he, int bpp); //=3);
-  ~CRawImage();
+	//! The default constructor requires width, height, and number of bytes per pixel. The latter is only allowed to be
+	//! one or three.
+	CRawImage(int wi, int he, int bpp);
 
-  //! Copy constructor
-  CRawImage(const CRawImage & other);
+	//! Default destructor, will free internal "data" if not null.
+	~CRawImage();
 
-  Pixel getPixel(int x, int y);
+	//! Copy constructor
+	CRawImage(const CRawImage & other);
 
-  Pixel getPixel(int x, int y, Patch & patch);
+	//! Only valid if bpp=3, returns an rgb pixel
+	Pixel getPixel(int x, int y);
 
-  void setPixel(int x, int y, Patch & patch, Pixel pixel);
+	//! Returns a pixel in a given patch
+	Pixel getPixel(int x, int y, Patch & patch);
 
-  void getPatch(int x, int y, Patch & patch);
+	//! Set a pixel in a given patch
+	void setPixel(int x, int y, Patch & patch, Pixel pixel);
 
-  CRawImage* patch2Img(Patch &patch);
+	//! Get the patch itself (requires a malloc op)
+	void getPatch(int x, int y, Patch & patch);
 
-  void refresh();
+	//! Add header information etc. required to make a full-fledged CRawImage from a Patch struct
+	CRawImage* patch2Img(Patch &patch);
 
-  void saveBmp(const char* name);
+	//! Reallocate internal data structures if necessary
+	void refresh();
 
-  //! With multiple instances of class CRawImage, they will overwrite each other
-  void saveNumberedBmp(const char* name, bool increment = true);
+	//! Save to file, filename will automatically be appended by index
+	void saveBmp(const char* name);
 
-  bool loadBmp(const char* name);
-  void swap();
+	//! With instances of class CRawImage across multiple binaries, the images will overwrite each other
+	void saveNumberedBmp(const char* name, bool increment = true);
 
-  void updateHeader();
+	//! Get the last file name
+	int getSaveNumber();
 
-  void plotLine(int x,int y);
-  void plotCenter();
+	//! Get the last number/index of saved file
+	static int numSaved;
 
-  void plotCross(int i,int j,int size);
+	//! Load the image from file again, no index will be appended to the name
+	bool loadBmp(const char* name);
 
-  int  getSaveNumber();
+	//! Swap the R and B channel if the image contains RGB values
+	void swap();
 
-  inline bool isMonochrome() { return (bpp == 1); }
+	//! Set the right BMP header, depends on dimensions, bpp, etc.
+	void updateHeader();
 
-  /**
-   * Be careful, this really throws information away. The grayscale values are created
-   * according to human sensitivity values (30, 59, 11).
-   */
-  void makeMonochrome();
+	//! Plot a vertical and/or horizontal line through x and y (not entirely trivial if bpp > 1)
+	void plotLine(int x,int y);
+	//! Plot blob in center of image
+	void plotCenter();
+	//! Plot a cross of size "size" at given position
+	void plotCross(int i,int j,int size);
 
-  /**
-   * Some as makeMonochrome, but keeps image intact, and stores result in parameter "result"
-   * @param result   Previously constructed image
-   */
-  void makeMonochrome(CRawImage* result);
+	//! Check if image is (already) monochrome
+	inline bool isMonochrome() { return (bpp == 1); }
 
-  double getOverallBrightness(bool upperHalf);
+	//! This really throws information away! The grayscale values are according to human sensitivity values (30, 59, 11)
+	void makeMonochrome();
 
-  static int numSaved;
+	/**
+	 * Same as makeMonochrome, but keeps image intact, and stores result in parameter "result"
+	 * @param result   Previously constructed image
+	 */
+	void makeMonochrome(CRawImage* result);
 
-//  int palette;
+	//! Get the brightness by iterating over all pixels (expensive)
+	double getOverallBrightness(bool upperHalf);
 
-//  unsigned char* getData() { return data; }
-//private:
+	//! Just show the data to the user, if you screw up, it's your own responsibility
+	unsigned char* data;
 
-  unsigned char* data;
+	//! Reallocate the internal data structure on changing the number of bytes per pixel
+	void setbpp(int bpp) { this->bpp = bpp; refresh(); }
 
-  //! Make sure the data array is adjusted, by adding setters
-  void setbpp(int bpp) { this->bpp = bpp; refresh(); }
-  void setdimensions(int width, int height) { this->width = width; this->height = height; refresh(); }
+	//! Reallocate the internal data structure on changing the image dimensions
+	void setdimensions(int width, int height) { this->width = width; this->height = height; refresh(); }
 
-  const int getwidth() { return width; };
-  const int getheight() { return height; };
-  const int getsize() { return size; };
+	//! Get the width in pixels
+	const inline int getwidth() { return width; };
+
+	//! Get the height in pixels
+	const inline int getheight() { return height; };
+
+	//! Get the size in memory (pixels times the bytes per pixel)
+	const inline int getsize() { return size; };
 private:
-  int width;
-  int height;
-  int bpp;
-  int size;
+	//! Width of image in pixels
+	int width;
+	//! Height of image in pixels
+	int height;
+	//! Bytes per pixel
+	int bpp;
+	//! Size of data structure (pixels times the bytes per pixel)
+	int size;
 };
 
 #endif
